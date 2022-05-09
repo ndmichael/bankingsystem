ad78030ae2d2e45f0ade26877a81cff606abd482
michael codesmith <up811786@student.reading.ac.uk>
2022-05-09 21:52:32 +0100

    updated views.py

--- ibanking/views.py
+++ ibanking/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'ibanking/index.html')
+
+def charity(request):
+    return render(request, 'ibanking/charity.html')
