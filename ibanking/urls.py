51d78e9abdf5011d31acbacaf4688792a021b0c8
michael codesmith <up811786@student.reading.ac.uk>
2022-05-09 21:52:08 +0100

    updated charity view url

--- ibanking/urls.py
+++ ibanking/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, charity
 
 
 
 urlpatterns = [
     path('', index, name="index"),
+    path("charity/", charity, name="charity"),
 ]
