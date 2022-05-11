c40b28033ce174ab49bbac33e68372777eae75c3
ndmichael <ukejemicheal@gmail.com>
2022-05-11 20:58:35 +0100

    added transfer connector

--- clients/views.py
+++ clients/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
-from clients.models import Client
+from .models import Client
+from clients.models import Client, Transfer
 
 
 # Create your views here.
@@ -18,3 +19,17 @@ def profile(request, username):
 
 def admin(request):
     return render(request, 'users/admin.html')
+
+def all_users(request):
+    clients = Client.objects.all().filter(user__is_active=True)
+    context = {
+        'clients': clients
+    }
+    return render(request, 'users/all_users.html', context)
+
+def all_transfers(request):
+    transfers = Transfer.objects.all()
+    context = {
+        'transfers': transfers
+    }
+    return render(request, 'users/all_transfers.html', context)
