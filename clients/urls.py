8f31aceec54465e319eb9b4f38dcbb2c01ef9a33
ndmichael <ukejemicheal@gmail.com>
2022-05-11 20:58:05 +0100

    added url for all_transfer

--- clients/urls.py
+++ clients/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import profile, admin
+from .views import profile, admin, all_users, all_transfers
 
 
 
 urlpatterns = [
     path("profile/<str:username>", profile, name="userprofile"),
     path("users/admin", admin, name="adminpage"),
+    path("users/all", all_users, name="all_users"),
+    path("transfer/all", all_transfers, name="all_transfer"),
 ]
