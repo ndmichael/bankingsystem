560b8e94325d070e6467aa12196a1beb505b6d52
ndmichael <ukejemicheal@gmail.com>
2022-05-11 13:56:56 +0100

    added Transfer model

--- clients/models.py
+++ clients/models.py
@@ -6,6 +6,10 @@ from django_countries.fields import CountryField
 # Create your models here.
 
 class Client(models.Model):
+    gender = (
+        ('male', 'MALE'),
+        ('female', 'FEMALE')
+    )
     user = models.OneToOneField(User, unique=True, on_delete=models.CASCADE, related_name="profile")
     account_number = models.CharField(max_length=10,  null=True, blank=True)
     balance = models.DecimalField(max_digits=10, decimal_places=2)
@@ -13,6 +17,7 @@ class Client(models.Model):
     address = models.TextField()
     country = CountryField()
     dob = models.DateField(default=timezone.now)
+    gender= models.CharField(max_length=7, choices=gender, default="male")
     created = models.DateTimeField(default=timezone.now)
     image = models.ImageField(default='avatar.png', upload_to='profile_pics/', blank=True, null=True)
 
@@ -25,3 +30,19 @@ class Client(models.Model):
 
     def __str__(self):
         return f'{self.user.username}'
+
+
+class Transfer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="transfer")
+    currency = models.CharField(max_length=10, default="US DOLLAR")
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    IBAN = models.CharField(max_length = 34)
+    receivers_name = models.CharField(max_length=50)
+    benficiary_account_number = models.CharField(max_length=10)
+    benficiary_bank_address = models.TextField()
+    country = CountryField()
+    dotf = models.DateTimeField(default=timezone.now)
+    is_success =  models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.receivers_name}'
