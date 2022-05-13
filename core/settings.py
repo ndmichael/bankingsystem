a67ed5ac87d2300464df2cb2611adf6d42fe9038
ndmichael <ukejemicheal@gmail.com>
2022-05-13 07:32:38 +0100

    updated settings

--- core/settings.py
+++ core/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-1fqryc($ii8hjr0(thfoa$z$9qr8st#h8&7pp1i-w@t%g&z8o9'
-SECRET_KEY =  os.environ.get("SECRET_KEY_SDELIVERY")
+SECRET_KEY =  os.environ.get("SECRET_KEY_BOIW")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG_VALUE") == "True"
