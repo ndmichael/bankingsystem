d700ac965b7bc7f7dea027e4d4899a7ec15b6926
ndmichael <ukejemicheal@gmail.com>
2022-09-17 03:42:31 +0100

    added psycopy2

--- core/settings.py
+++ core/settings.py
@@ -96,8 +96,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+
     }
 }
 
