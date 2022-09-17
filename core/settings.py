80574e9eaebd5492bef56097401646b609f40dc7
ndmichael <ukejemicheal@gmail.com>
2022-09-17 04:11:52 +0100

    added csrf trusted origin

--- core/settings.py
+++ core/settings.py
@@ -108,6 +108,8 @@ DATABASES = {
     }
 }
 
+CSRF_TRUSTED_ORIGINS = ['https://boiworldwide.com', 'http://boiworldwide.com']
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
