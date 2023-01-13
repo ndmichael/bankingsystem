53ea313607a592ed251b81e4b678804d40a61147
ndmichael <ukejemicheal@gmail.com>
2023-01-13 13:16:27 +0100

    working with progress

--- core/settings.py
+++ core/settings.py
@@ -96,14 +96,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        # 'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': 'railway',
-        # 'USER': 'postgres',
-        # 'PASSWORD': 'XjwA4GajylCH5dhNmgCD',
-        # 'HOST': 'containers-us-west-90.railway.app',
-        # 'PORT': '7772',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'XjwA4GajylCH5dhNmgCD',
+        'HOST': 'containers-us-west-90.railway.app',
+        'PORT': '7772',
 
     }
 }
